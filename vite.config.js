03cdc764e2a110a9ba8cf7e17cfa25ebce7049ad
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2025-03-14 01:56:19 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      yup: 'yup/dist/yup.esm.js',
+    },
+  },
   optimizeDeps: {
     include: ['yup'],
   },
